b4c747ea2d14c179f99878c5bbb89c681b4f0115
nirmal kumar <nirmal.kumar@qpaix.com>
2024-03-05 12:13:03 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar } from "./components/common/Navbar";
+import { Footer } from "./components/layout/Footer";
+import { Home } from "./components/pages/home";
+import { About } from "./components/pages/aboutUs";
+import "./components/styles/global.css";
+import { Products } from "./components/pages/products";
+import { Services } from "./components/pages/services";
+import { Contact } from "./components/pages/contactus";
+export const App = () => {
+  return (
+    <div className="app">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/contact-us" element={<Contact />} />
+          <Route path="/services" element={<Services />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
+  );
+};
